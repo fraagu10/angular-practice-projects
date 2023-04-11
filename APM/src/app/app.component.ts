5762cf82271b2d37bac3f2ad6b0a41069a446eb7
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-11 15:01:27 -0500

    converted product names to links that are unique urls

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Franco\'s House Shop of Wonders';
+  title = 'ACME Production Products';
 }
