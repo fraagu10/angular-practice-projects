20296c0d7ec5fe553fe5fd6e8bd310425634adf6
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-11 22:34:39 -0500

    refactored products feature into a module

---
+++ APM/src/app/products/product.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailsComponent } from './product-details.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsGuard } from './product-details.guard';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailsComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailsGuard],
+        component: ProductDetailsComponent
+      },
+    ])
+  ]
+})
+export class ProductModule { }
