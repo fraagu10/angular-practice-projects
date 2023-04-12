1f0fb190776bf7fca3ea034b91a14721134acdea
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-11 22:52:18 -0500

    created shared module that can be imported into future feature modules

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailsComponent } from './product-details.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailsGuard } from './product-details.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailsComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       {
@@ -25,7 +20,8 @@ import { ProductDetailsGuard } from './product-details.guard';
         canActivate: [ProductDetailsGuard],
         component: ProductDetailsComponent
       },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
