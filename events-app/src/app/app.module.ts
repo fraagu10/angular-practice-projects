15eaae808151c46ff581f294f540c665924bdd9d
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-20 12:38:47 -0500

    created event list and event thumbnail components to pass information between one another

--- events-app/src/app/app.module.ts
+++ events-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
